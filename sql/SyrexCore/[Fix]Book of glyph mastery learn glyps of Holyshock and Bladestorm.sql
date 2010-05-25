c21483699034f1406963925639c81e7f3b5d2f26
Syrex <syrexwow@yandex.ru>
2010-05-26 00:24:29 +0300

    Merge

---
+++ sql/SyrexCore/[Fix]Book of glyph mastery learn glyps of Holyshock and Bladestorm.sql
@@ -0,0 +1,2 @@
+INSERT INTO skill_discovery_template VALUES
+(63945,64323,0,100),(63870,64323,0,100);
---
+++ src/shared/revision.h
@@ -0,0 +1,12 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _BUILD_DIRECTIVE "Release"
+ #define _REVISION "8290"
+ #define _HASH "0d0854e7bcc3"
+ #define _REVISION_DATE "*"
+ #define _REVISION_TIME "*"
+ #define FILEVER        0,0,8290,0
+ #define PRODUCTVER     0,0,8290,0
+ #define STRFILEVER     "0, 0, 8290, 0d0854e7bcc3"
+ #define STRPRODUCTVER  "0, 0, 8290, 0d0854e7bcc3"
+#endif // __REVISION_H__
